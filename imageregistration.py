b49a83480f3168b99979c550b1696259c913422c
Clayton Pruitt <clay10pruitt@gmail.com>
2020-04-02 22:36:54 -0400

    Created Python3 script that will eventually be used for image registration once I finish it.

---
+++ imageregistration.py
@@ -0,0 +1,11 @@
+"""
+:file: imageregistration.py
+:author: clayton pruitt
+:date: 4/2/2020
+:desc: Computes image registration for all photos in a given directory.
+"""
+
+import cv2
+import numpy as np
+import sys
+    
